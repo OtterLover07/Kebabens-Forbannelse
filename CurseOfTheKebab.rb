2d0667a5b6c9b19082366eb5ad5a9fa40d5419f7
OtterLover07 <melker@willforss.se>
2025-04-22 13:13:17 +0200

    Update CurseOfTheKebab.rb

--- CurseOfTheKebab.rb
+++ CurseOfTheKebab.rb
@@ -1,12 +1,57 @@
+puts "\e[H\e[2J"
+puts "Loading..."
 
+class Item
+    @held = false
+    @used = false
+
+    def initialize(name, location, usable_with)
+        @name = name
+        @location = location
+        @usable_with = usable_with
+        $items << name
+    end
+
+    def name
+        return @name
+    end
+
+    def location
+        return @loaction
+    end
+
+    def pick_up
+        if $plats != @location || @used
+            puts "Det finns ingen " + name + " här."
+        elsif @held
+            puts "Du har redan " + name + "."
+        else
+            @held = true
+            puts "Du plockar upp " + name + "."
+        end
+        return false
+    end
+
+    def use(target)
+        if !@held
+            puts "Du har inte " + @name + "."
+        elsif target == @usable_with || @usable_with.include?(target)
+            @held = false
+            @used = true
+            return true
+        else
+            puts "Du försöker använda " + @name + " med " + target + ", men det funkar inte."
+        end
+        return false
+    end
+end
 
 #global variabel
+$items = []
 $plats = "järntorget"
-$inventory = []
-$history = []
 $life = 100
 $hunger = 100
-$loop = 0
+$money = 15
 
 
 def init()
@@ -20,11 +65,12 @@ end
 #FÅ HANDLINGSTRÄDET ATT FUNKA
 
 def järntorget()
-   puts "vart vill du gå?"
+   puts "Vad vill du göra?"
    user_input = gets.chomp
    while user_input != "quit"
        if user_input == "gå kebabvagnen"
            $plats = "kebabvagnen"
+           puts "Du går till kebabvagnen."
            return #OBS VIKTIGT
        elsif user_input == "prata lisebergskaninen"
            $plats = "lisebergskaninen"
@@ -39,6 +85,32 @@ def järntorget()
    return
 end
 
+####################### FIXA ALLMÄN KOD - SLIPPA HARDCODA ##########################
+def default()
+    while true
+        puts "Vad vill du göra?"
+        puts ""
+        user_input = gets.chomp.split
+        while user_input[0] != "quit"
+            if user_input[0] == "ta"
+                $items.const_get(user_input[1])
+                
+            elsif user_input[0] == "köp" && user_input[1] == "kebab"
+                
+            elsif user_input[0] == "använd"
+               
+            elsif user_input[0] == "poor"
+                
+            else
+                puts "ogiltig handling"
+            end
+            puts ""
+            puts "Vad vill du göra?"
+            puts ""
+            user_input = gets.chomp.split
+        end
+        exit
+end
 
 def main()
    init()
@@ -57,5 +129,52 @@ def main()
 end
 
 
-main()
+##################TESTKOD NEDANFÖR############################
+baby = Item.new("baby", "järntorget", "fönster")
+kebab = Item.new("kebab", "järntorget", ["fönster","mig"])
+
+puts "Loading complete!"
+puts "\e[H\e[2J"
+
+while true
+    puts "Vad vill du göra?"
+    puts ""
+    user_input = gets.chomp.split
+    while user_input[0] != "quit"
+        if user_input[0] == "ta"
+            if user_input[1] == "baby"
+                baby.pick_up
+            elsif user_input[1] == "kebab"
+                kebab.pick_up
+            end
+        elsif user_input[0] == "köp" && user_input[1] == "kebab"
+            if $money >= 5
+                $money -= 5
+                kebab.pick_up
+            else
+                puts "Du har inte råd att köpa kebab."
+            end
+        elsif user_input[0] == "använd"
+            if user_input[1] == "baby"
+                if baby.use(user_input[2])
+                    puts "NooOooOo dOn'T tHrOw ThA bAbY!"
+                end
+            elsif user_input[1] == "kebab"
+                if kebab.use(user_input[2])
+                    puts "Du använder kebaben. Du är nu död. Grattis."
+                end
+            end
+        elsif user_input[0] == "poor"
+            $money = 0
+            puts "Du är nu fattig."
+        else
+            puts "ogiltig handling"
+        end
+        puts ""
+        puts "Vad vill du göra?"
+        puts ""
+        user_input = gets.chomp.split
+    end
+    exit
+end
 
